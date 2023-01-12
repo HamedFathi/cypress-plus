12aeeb3f86506dd7b31b86fe9e366c058c2c3d5b
hamedfathi <hamedfathi@outlook.com>
2023-01-12 13:27:00 +0100

    feat(Project): more commands added.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -9,23 +10,246 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 
-Cypress.Commands.add("dataCy", (selector, ...args) => {
-    if (selector) {
-        return cy.get(`[data-cy="${selector}"]`, ...args);
-    }
+Cypress.Commands.add("getByDataCy", (selector: string, options?: any) => {
+    return cy.get(`[data-cy="${selector}"]`, options);
 });
 
-Cypress.Commands.add("dataCyAdv", (selector, moreSelectors, ...args) => {
-    if (selector) {
-        moreSelectors = moreSelectors ?? "";
-        return cy.get(`[data-cy="${selector}"] ${moreSelectors}`.trim(), ...args);
-    }
+Cypress.Commands.add("getByData", (dataName: string, selector: string, options?: any) => {
+    return cy.get(`[data-${dataName}="${selector}"]`, options);
 });
 
-Cypress.Commands.add("promise", (promise: Promise<any>) => {
+Cypress.Commands.add("getByDataAdv", (dataName: string, selector: string, moreSelectors: string, options?: any) => {
+    moreSelectors = moreSelectors ?? "";
+    return cy.get(`[data-${dataName}="${selector}"] ${moreSelectors}`.trim(), options);
+});
+
+Cypress.Commands.add("getByDataCyAdv", (selector: string, moreSelectors: string, options?: any) => {
+    moreSelectors = moreSelectors ?? "";
+    return cy.get(`[data-cy="${selector}"] ${moreSelectors}`.trim(), options);
+});
+
+Cypress.Commands.add("promise", <T>(promise: Promise<T>) => {
     return cy.then(() => promise);
 });
 
-Cypress.Commands.add("promiseOfT", <T>(promise: Promise<T>) => {
-    return cy.then(() => promise);
+Cypress.Commands.add("waitForUrlToChange", (currentUrl: string, timeout: number) => {
+    if (timeout <= 0)
+        timeout = 0;
+    cy.wait(timeout);
+    return cy.url().should("not.eq", currentUrl);
+});
+
+Cypress.Commands.add("scrollTo", (selector: string, options?: any) => {
+    return cy.get(selector, options).scrollIntoView();
+});
+Cypress.Commands.add("assertElementsCount", (selector: string, count: number, lengthComparison: "equal" | "above" | "below" | "atMost" | "atLeast", options?: any) => {
+    if (count <= 0)
+        count = 0;
+    switch (lengthComparison) {
+        case "equal":
+            return cy.get(selector, options).should('have.length', count);
+        case "above":
+            return cy.get(selector, options).should('have.length.above', count);
+        case "below":
+            return cy.get(selector, options).should('have.length.below', count);
+        case "atLeast":
+            return cy.get(selector, options).should('have.length.at.least', count);
+        case "atMost":
+            return cy.get(selector, options).should('have.length.at.most', count);
+    }
+});
+Cypress.Commands.add("getCount", (selector: string, options?: any) => {
+    return cy.get(selector, options).then($elements => {
+        let countOfElements = $elements.length;
+        return cy.wrap(countOfElements);
+    });
+});
+
+Cypress.Commands.add("getByAriaLabel", (ariaLabel: string, options?: any) => {
+    return cy.get(`[aria-label="${ariaLabel}"]`, options);
+});
+
+Cypress.Commands.add("getByTitle", (title: string, options?: any) => {
+    return cy.get(`[title="${title}"]`, options);
+});
+
+Cypress.Commands.add("getByAlt", (alt: string, options?: any) => {
+    return cy.get(`[alt="${alt}"]`, options);
+});
+
+Cypress.Commands.add("getByPlaceholder", (placeholder: string, options?: any) => {
+    return cy.get(`[placeholder="${placeholder}"]`, options);
+});
+
+Cypress.Commands.add("getByValue", (value: string, options?: any) => {
+    return cy.get(`[value="${value}"]`, options);
+});
+
+Cypress.Commands.add("waitAndClick", (selector: string, timeout: number) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(selector, { timeout: timeout }).click();
+});
+
+Cypress.Commands.add("getByText", (text: string, timeout: number) => {
+    return cy.contains(text, { matchCase: false, includeShadowDom: true, timeout: timeout });
+});
+
+Cypress.Commands.add("getByExactText", (text: string, timeout: number) => {
+    return cy.contains(text, { matchCase: true, includeShadowDom: true, timeout: timeout });
+});
+
+Cypress.Commands.add("clearAndType", (selector: string, text: string, pressEnter: boolean = false, options?: any) => {
+    if (pressEnter)
+        return cy.get(selector, options).clear().type(text + '{enter}')
+    return cy.get(selector, options).clear().type(text);
+});
+Cypress.Commands.add("waitForClearAndType", (selector: string, text: string, timeout: number, pressEnter: boolean = false, options?: any) => {
+    if (timeout <= 0)
+        timeout = 0;
+    cy.wait(timeout);
+    if (pressEnter)
+        return cy.get(selector, options).clear().type(text + '{enter}')
+    return cy.get(selector, options).clear().type(text);
+});
+Cypress.Commands.add("selectFromDropdown", (selector: string, value: string, options?: any) => {
+    return cy.get(selector, options).select(value);
+});
+
+Cypress.Commands.add("hover", (selector: string, options?: any) => {
+    return cy.get(selector, options).trigger("mouseover");
+});
+Cypress.Commands.add("waitForElement", (selector: string, timeout: number) => {
+    return cy.get(selector, { timeout: timeout });
+});
+Cypress.Commands.add("waitForElementDataCy", (selector: string, timeout: number) => {
+    return cy.get(`[data-cy="${selector}"]`, { timeout: timeout });
+});
+Cypress.Commands.add("waitForElementDataCyAdv", (selector: string, moreSelectors: string, timeout: number) => {
+    moreSelectors = moreSelectors ?? "";
+    return cy.get(`[data-cy="${selector}"] ${moreSelectors}`.trim(), { timeout: timeout });
+});
+
+Cypress.Commands.add("getInputValue", (selector: string, options?: any) => {
+    return cy.get(selector, options).then(($input) => {
+        return cy.wrap($input.val());
+    });
+});
+Cypress.Commands.add("getParent", (selector: string, options?: any) => {
+    return cy.get(selector, options).parent();
+});
+
+Cypress.Commands.add("getSibling", (selector: string, nth: number, options?: any) => {
+    return cy.get(selector, options).siblings(':nth-child(' + nth + ')');
+});
+Cypress.Commands.add("getNthChild", (selector: string, nth: number, options?: any) => {
+    return cy.get(selector, options).children().eq(nth);
+});
+Cypress.Commands.add("checkCheckbox", (selector: string, options?: any) => {
+    return cy.get(selector, options).check();
+});
+Cypress.Commands.add("uncheckCheckbox", (selector: string, options?: any) => {
+    return cy.get(selector, options).uncheck();
+});
+Cypress.Commands.add("getByClass", (className: string, options?: any) => {
+    return cy.get(`.${className}`, options);
+});
+Cypress.Commands.add("clickButton", (buttonText: string, options?: any) => {
+    return cy.get(`button:contains("${buttonText}")`, options).click();
+});
+Cypress.Commands.add("clickLink", (linkText: string, options?: any) => {
+    return cy.get(`link:contains("${linkText}")`, options).click();
+});
+Cypress.Commands.add("getByRole", (role: string, options?: any) => {
+    return cy.get(`[role=${role}]`, options);
+});
+Cypress.Commands.add("getByName", (name: string, options?: any) => {
+    return cy.get(`[name=${name}]`, options);
+});
+Cypress.Commands.add("getByHref", (href: string, options?: any) => {
+    return cy.get(`a[href='${href}']`, options);
+});
+Cypress.Commands.add("getFirst", (selector: string, options?: any) => {
+    return cy.get(selector, options).first();
+});
+Cypress.Commands.add("getLast", (selector: string, options?: any) => {
+    return cy.get(selector, options).last();
+});
+Cypress.Commands.add("getFirstNth", (selector: string, nth: number, options?: any) => {
+    return cy.get(selector, options).first().nextAll().eq(nth);
+});
+Cypress.Commands.add("getLastNth", (selector: string, nth: number, options?: any) => {
+    return cy.get(selector, options).last().prevAll().eq(nth);
+});
+Cypress.Commands.add("focus", (selector: string, options?: any) => {
+    return cy.get(selector, options).focus();
+});
+Cypress.Commands.add("getByAriaDescribedBy", (ariaDescribedBy: string, options?: any) => {
+    return cy.get(`[aria-describedby='${ariaDescribedBy}']`, options);
+});
+Cypress.Commands.add("getByAriaControls", (ariaControls: string, options?: any) => {
+    return cy.get(`[aria-controls='${ariaControls}']`, options);
+});
+Cypress.Commands.add("getByAriaCurrent", (ariaCurrent: string, options?: any) => {
+    return cy.get(`[aria-current='${ariaCurrent}']`, options);
+});
+
+Cypress.Commands.add("waitForInvisible", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(selector, { timeout: timeout }).should('not.be.visible');
+});
+Cypress.Commands.add("waitForVisible", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(selector, { timeout: timeout }).should('be.visible');
+});
+Cypress.Commands.add("waitForInvisibleDataCy", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(`[data-cy="${selector}"]`, { timeout: timeout }).should('not.be.visible');
+});
+Cypress.Commands.add("waitForVisibleDataCy", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(`[data-cy="${selector}"]`, { timeout: timeout }).should('be.visible');
+});
+
+Cypress.Commands.add("waitForExist", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(selector, { timeout: timeout }).should('exist');
+});
+Cypress.Commands.add("waitForNotExist", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(selector, { timeout: timeout }).should('not.exist');
+});
+Cypress.Commands.add("waitForExistDataCy", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(`[data-cy="${selector}"]`, { timeout: timeout }).should('exist');
+});
+Cypress.Commands.add("waitForNotExistDataCy", (selector: string, timeout) => {
+    if (timeout <= 0)
+        timeout = 0;
+    return cy.get(`[data-cy="${selector}"]`, { timeout: timeout }).should('not.exist');
+});
+
+Cypress.Commands.add("navigateTo", (route: string) => {
+    return cy.visit(route);
+});
+Cypress.Commands.add("goBack", () => {
+    return cy.go("back");
+});
+Cypress.Commands.add("goForward", () => {
+    return cy.go("forward");
+});
+
+Cypress.Commands.add("clearSessionAndCookies", () => {
+    return cy.window().then((win) => {
+        win.sessionStorage.clear();
+        cy.clearCookies();
+        cy.clearLocalStorage();
+    });
 });
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,13 +1,77 @@
 declare module Cypress {
     interface Chainable {
-        dataCyAdv<E extends Node = HTMLElement>(
-            selector: string,
-            moreSelectors: string,
-            ...args: any
-        ): Chainable<JQuery<E>>;
-        dataCy<E extends Node = HTMLElement>(selector: string, ...args: any): Chainable<JQuery<E>>;
-        promise(promise: Promise<any>): Chainable<any>;
-        promiseOfT<T>(promise: Promise<T>): Chainable<T>;
-    
+        getByDataCy<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByDataCyAdv<E extends Node = HTMLElement>(selector: string, moreSelectors: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+
+        getByData<E extends Node = HTMLElement>(dataName: string, selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByDataAdv<E extends Node = HTMLElement>(dataName: string, selector: string, moreSelectors: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+
+        promise<T>(promise: Promise<T>): Chainable<T>;
+
+        waitForUrlToChange(currentUrl: string, timeout: number): Chainable<string>;
+
+        scrollTo(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+
+        getByAriaDescribedBy<E extends Node = HTMLElement>(ariaLabel: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByAriaControls<E extends Node = HTMLElement>(ariaLabel: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByAriaCurrent<E extends Node = HTMLElement>(ariaLabel: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+
+        getByAriaLabel<E extends Node = HTMLElement>(ariaLabel: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByTitle<E extends Node = HTMLElement>(title: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByAlt<E extends Node = HTMLElement>(alt: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByPlaceholder<E extends Node = HTMLElement>(placeholder: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByValue<E extends Node = HTMLElement>(value: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByClass<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByRole<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getByName<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+
+        waitAndClick(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        getByText(selector: string, timeout: number): Chainable<any>;
+        getByExactText(selector: string, timeout: number): Chainable<any>;
+        clearAndType(selector: string, text: string, pressEnter?: boolean, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        waitForClearAndType(selector: string, text: string, timeout: number, pressEnter?: boolean, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+
+        getCount(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<number>;
+
+        clearSessionAndCookies(): Chainable<Cypress.AUTWindow>;
+        goBack(): Chainable<Cypress.AUTWindow>;
+        goForward(): Chainable<Cypress.AUTWindow>;
+        navigateTo(route: string): Chainable<Cypress.AUTWindow>;
+
+        assertElementsCount(selector: string, count: number, lengthComparison: "equal" | "above" | "below" | "atMost" | "atLeast", options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+
+        getInputValue(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<string | number | string[] | undefined>;
+
+        getParent<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getSibling<E extends Node = HTMLElement>(selector: string, nth: number, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getNthChild<E extends Node = HTMLElement>(selector: string, nth: number, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getFirst(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        getLast<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getFirstNth<E extends Node = HTMLElement>(selector: string, nth: number, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        getLastNth<E extends Node = HTMLElement>(selector: string, nth: number, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+        focus(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+
+        checkCheckbox(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        uncheckCheckbox(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        clickButton(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        clickLink(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        getByHref<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<E>>;
+
+        waitForNotExistDataCy(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForNotExist(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForExistDataCy(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForExist(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForInvisibleDataCy(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForInvisible(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForVisibleDataCy(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForVisible(selector: string, timeout: number): Chainable<JQuery<HTMLElement>>;
+        waitForElement<E extends Node = HTMLElement>(selector: string, timeout: number): Chainable<JQuery<E>>;
+        waitForElementDataCy<E extends Node = HTMLElement>(selector: string, timeout: number): Chainable<JQuery<E>>;
+        waitForElementDataCyAdv<E extends Node = HTMLElement>(selector: string, moreSelectors: string, timeout: number): Chainable<JQuery<E>>;
+
+        selectFromDropdown(selector: string, value: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+        hover(selector: string, options?: Partial<Loggable & Timeoutable & Withinable & Shadow>): Chainable<JQuery<HTMLElement>>;
+
+
     }
 }
